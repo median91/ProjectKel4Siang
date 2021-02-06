56bb1c6ae68bd0852754c64e02bceadc154d843d
kael <dimaspraditya.dp@gmail.com>
2021-02-06 23:25:24 +0700

    add dev/mikaeldimas

--- models/item.js
+++ models/item.js
@@ -1,9 +1,19 @@
 const items = require('../database/items.json')
 
-const model = {
-    test: items,
-    item: items.id
 
+const model = {
+    item: items,
+    checkById: (id) => items.find(item => item.id === id),
+    create: (userId, name) => {
+        let id = items.length
+        let item = { id, userId, name }
+
+        items.push(item)
+
+        return item
+    },
+    findId: (id) => items.find(id => item.id === id),
 }
+
 
 module.exports = model
--- routes/item.js
+++ routes/item.js
@@ -1,16 +1,80 @@
-const { request } = require('express')
 const express = require('express')
-const itemmodel = require('../models/item')
+const itemModel = require('../models/item')
 const item = express.Router()
+const userModel = require('../models/user')
 
 
 item.get('/', (req, res) => {
-    res.send(itemmodel.test)
+
+    res.send(itemModel.item)
 })
 
 item.get('/:id', (req, res) => {
     const id = req.params.id
+    if (!id) {
+        res.status(400).json({
+            "success": false,
+            "message": "item id found.",
+            "data": {}
+        })
+    }
     res.send(id)
+})
+
+item.post('/', (req, res) => {
+    const { userId, name } = req.body
+    if (!name || !userId) {
+        res.status(400).json({
+            "success": false,
+            "message": "make sure you entered all required data.",
+            "data": {}
+        })
+        let item = itemModel.get(id)
+        res.status(200).json({
+            "success": 200,
+            "message": "Create success.",
+            "data": item
+        })
+    }
+    const user = userModel.checkById(userId)
+    console.log(userId);
+    if (!user) {
+        res.status(400).json({
+            "success": false,
+            "message": "user id not found.",
+            "data": {}
+        })
+    }
+    let item = itemModel.create(userId, name)
+    res.status(200).json({
+        "success": 200,
+        "message": "Create success.",
+        "data": item
+    })
+})
+
+item.put('/', (req, res) => {
+    const { name } = req.body
+    if (!name) {
+        res.status(400).json({
+            "success": false,
+            "message": "make sure you entered all required data.",
+            "data": {}
+        })
+    }
+
+})
+
+item.delete('/', (req, res) => {
+    const { name } = req.body
+    if (!name) {
+        res.status(400).json({
+            "success": false,
+            "message": "make sure you entered all required data.",
+            "data": {}
+        })
+    }
+
 })
 
 module.exports = item
