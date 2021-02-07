1b332a05ba4a16fe0b49aeda5584ee6f18797fe5
Jauza <jauza1998@gmail.com>
2021-02-07 11:49:38 +0700

    error function in indexJS

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 3000
 
 app.use(express.json());
 app.use(routes)
+app.use(function (error, req, res, next) {
+    res.send(error.message)
+})
 
 app.listen(port, () => {
     console.log(`App Running on port ${port}`)
