3f5969255e1c8a7cfba786c7fb4848ae44be816e
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-02-08 10:33:14 +0700

    Added new transaction validations for userId, itemId and friendId

--- routes/transaction.js
+++ routes/transaction.js
@@ -13,25 +13,29 @@ transaction.get('/', (req, res) => {
 transaction.get('/:id', (req, res) => {
     const id = Number(req.params.id)
     const result = transactionModel.findId(id)
-
+    
     if (result) {
+
         return res.status(200).json({
             "success": true,
             "message": "Transaction has been found successfully.",
             "data": result
         })
+
     } else {
+
         return res.status(404).json({
             "success": false,
             "message": "The transaction cannot be found.",
             "data": {}
-        })   
+        })
+
     }
 })
 
 // POST transaction into database.
 transaction.post('/', (req, res) => {
-    const {userId, friendId, itemId, nominal } = req.body
+    const {userId, friendId, itemId, nominal} = req.body
 
     // Check for empty input.
     if (!userId || !friendId || !itemId || !nominal) {
@@ -52,26 +56,49 @@ transaction.post('/', (req, res) => {
     }
 
     // Check for negative value.
-    if (userId > 0 && friendId > 0 && itemId > 0 && nominal > 0) {
-        // Create transaction.
-        const transaction = transactionModel.createTransaction(userId, friendId, itemId, nominal)
-        return res.status(200).json({
-            "success": true,
-            "message": "Transaction has been submitted successfully.",
-            "data": transaction
-        })
-    } else {
+    if (userId < 0 || friendId < 0 || itemId < 0 || nominal < 0) {
         return res.status(400).json({
             "success": false,
             "message": "Cannot create transaction. Negative value input is not allowed.",
             "data": {}
         })
     }
+
+    // Check for available userId, friendId and itemId.
+    const userIdResult = transactionModel.checkUserId(userId)
+    const itemIdResult = transactionModel.checkItemId(itemId)
+    const friendIdResult = transactionModel.checkFriendId(friendId)
+
+    if (!userIdResult || !itemIdResult || !friendIdResult) {
+        return res.status(400).json({
+            "success": false,
+            "message": "Cannot create transaction. User ID, Item ID or Friend ID not found.",
+            "data": {}
+        })
+    }
+
+    // Create transaction.
+    const transaction = transactionModel.createTransaction(userId, friendId, itemId, nominal)
+    return res.status(200).json({
+        "success": true,
+        "message": "Transaction has been submitted successfully.",
+        "data": transaction
+    })
 })
 
 // PUT
 transaction.put('/', (req, res) => {
     const { id, userId, friendId, itemId, nominal } = req.body
+
+    // Check for transaction Id.
+    const result = transactionModel.findId(id)
+    if (!result) {
+        return res.status(400).json({
+            "success": false,
+            "message": "Cannot edit transaction. The transaction id is not found.",
+            "data": {}
+        })
+    }
 
     // Check for empty input.
     if (!userId || !friendId || !itemId || !nominal) {
@@ -98,24 +125,28 @@ transaction.put('/', (req, res) => {
             "message": "Cannot edit transaction. Negative value input is not allowed.",
             "data": {}
         })
-    } 
+    }
 
-    // Edit the transaction.
-    const result = transactionModel.findId(id)
-    if (result) {
-        const transaction = transactionModel.editTransaction(id, userId, friendId, itemId, nominal)
-        return res.status(200).json({
-            "success": true,
-            "message": "Transaction has been editted successfully.",
-            "data": transaction
-        })
-    } else {
+    // Check for available userId, friendId and itemId.
+    const userIdResult = transactionModel.checkUserId(userId)
+    const itemIdResult = transactionModel.checkItemId(itemId)
+    const friendIdResult = transactionModel.checkFriendId(friendId)
+
+    if (!userIdResult || !itemIdResult || !friendIdResult) {
         return res.status(400).json({
             "success": false,
-            "message": "Cannot edit transaction. The transaction id is not found.",
+            "message": "Cannot edit transaction. User ID, Item ID or Friend ID not found.",
             "data": {}
         })
     }
+
+    // Edit the transaction.
+    const transaction = transactionModel.editTransaction(id, userId, friendId, itemId, nominal)
+        return res.status(200).json({
+            "success": true,
+            "message": "Transaction has been edited successfully.",
+            "data": transaction
+        })
 })
 
 // DELETE
